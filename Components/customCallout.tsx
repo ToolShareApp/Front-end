ff87fcd6c5abda4b720af0078023e86ac9c5d451
Sam Harradine <124873582+Equallizerr1@users.noreply.github.com>
2024-02-29 11:03:47 +0000

    added routing

--- Components/customCallout.tsx
+++ Components/customCallout.tsx
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useContext, useState } from "react";
 import { View, StyleSheet, Dimensions, Image, Text } from "react-native";
 import { Callout } from "react-native-maps";
 import { MarkerWithMetadata } from "./Map";
 import { GreenTheme } from "../Themes/GreenTheme";
+import GlobalStateContext from "../Contexts/GlobalStateContext";
+import { useNavigation, useRoute } from "@react-navigation/native";
 
 const screenWidth = Dimensions.get("window").width;
 
@@ -10,8 +12,63 @@ const CustomCallout: React.FC<{
 	marker: MarkerWithMetadata;
 	listingArray: any;
 }> = ({ marker, listingArray }) => {
+	const { api, user } = useContext(GlobalStateContext);
+	const [toolDetails, setToolDetails] = useState<object>();
+	const route = useRoute();
+	const navigation = useNavigation();
+
+	const { listing_id } = route.params;
+
+	const createNewChat = () => {
+		return api.post("/chat/", {
+			listingId: listing_id,
+			userId: user?.profile_id,
+		});
+	};
+
+	function startChat() {
+		const owner_id: number = toolDetails?.owner_id;
+		if (toolDetails?.owner_id === user?.profile_id) {
+			alert("Sorry, you can't create a chat with yourself");
+			return;
+		}
+		createNewChat()
+			.then((response) => {
+				if (response?.data?.recordId) {
+					const recordId = response?.data?.recordId;
+
+					console.log({
+						user_id: owner_id,
+						title: ownerName,
+						tool_name: toolName,
+						listing_id,
+						recordId,
+					});
+					// @ts-ignore
+					navigation.navigate("Messages", {
+						screen: "ChatScreen",
+						params: {
+							user_id: owner_id,
+							title: ownerName,
+							tool_name: toolName,
+							listing_id,
+							recordId,
+						},
+					});
+				}
+			})
+			.catch((error) => {
+				setToast(true);
+				console.error(error);
+			});
+	}
+
 	return (
-		<Callout tooltip>
+		<Callout
+			tooltip
+			onPress={() => {
+				startChat();
+			}}>
 			<View>
 				<View style={styles.container}>
 					<View style={{ paddingHorizontal: 16, paddingVertical: 8, flex: 1 }}>
